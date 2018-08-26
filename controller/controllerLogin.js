bdfb17f67270e033f9b7596e3cd7262864b5e1ff
PaoDLR <36217674+PaoDLR@users.noreply.github.com>
2018-08-26 20:00:03 +0800

    oops
    fixed the redirect lang on controllerLogin

--- controller/controllerLogin.js
+++ controller/controllerLogin.js
@@ -21,7 +21,7 @@ server.post('/login', function(req, resp){
   loginModel.checkLogin(req.body.user, req.body.pass, function(result){
       if(result)
           //This should advance and go to home page with the posts generated.
-          resp.redirect('/.pages.index');
+          resp.redirect('/.pages/index');
       else
           //This should go back to sign-in cause user failed to login successfully.
           resp.redirect('./pages/signin');
