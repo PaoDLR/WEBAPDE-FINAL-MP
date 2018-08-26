6403066f5883debf6b057c3dfb73af994136869c
tadhg <tadhgmccarthy03@gmail.com>
2018-08-26 23:37:27 +0800

    took out consoles

--- controller/postController.js
+++ controller/postController.js
@@ -8,13 +8,10 @@ function PostModule(server){
         var passDataLogin, passDataPost;
         
         postModel.allPosts(function(list){
-            console.log(list + "Post data-----------------------------------");
             passDataPost = list;
-            console.log(passDataPost + "here here here here");
         });
         
         loginModel.allUsers(function(list){
-            console.log(list + "user data----------------------------------------");
             passDataLogin = list;
             
             if(passDataLogin != undefined && passDataPost != undefined)
