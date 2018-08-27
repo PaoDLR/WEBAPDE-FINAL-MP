0232b4872b1328324ebdd9faadab3b95746a5433
Alex <alexandra_ysabel_reyes@dlsu.edu.ph>
2018-08-28 03:37:31 +0800

    edit and delete comments

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,4 +1,6 @@
 const commentModel = require('../model/commentModel');
+const postModel = require('../model/postModel');
+const loginModel = require('../model/loginModel');
 
 function CommentModule(server){
     
@@ -8,23 +10,36 @@ function CommentModule(server){
         
         commentModel.editComment(req.query.comment, req.query.editComment, function(comment){
             passDataComment = comment;
-            console.log(passDataComment +"this is passDataComment ----------------------------------------");
+            console.log(passDataComment +" <--------------------- this is passDataComment");
             resp.render('./pages/editResult',{ data:passDataComment });
         });
     });
     
-    
     server.get('/delete-comment', function(req, resp){
-        
+        console.log("sup---------------");
         var passDataComment;
+        var passDataLogin; 
+        var allPostDataData;
         
-            commentModel.deleteComment(req.query.comment, function(comment){
-            passDataComment = comment; 
-            console.log(passDataComment + "delete dis comment -----------------------");
-            resp.render('./pages/post', { data:passDataComment});
+         commentModel.findCommentbyID(req.query.id, function(comment){
+            passDataComment = comment;
+            
+            commentModel.deleteComment(req.query.id);
         });
+        
+        postModel.allPosts(function(list){
+            allPostDataData = list;
+        });
+        
+        console.log(passDataComment + " <--------- deleted comment");
+        
+        loginModel.findUser(req.session.user, function(list){
+           passDataLogin = list;
+            console.log(passDataLogin + " <---- loginData ");
+            resp.render('./pages/post', {loginData: passDataLogin, data: allPostDataData, commentData: passDataComment, loggedIn: req.session.user});
+        });
+        
     });
-
 }
 
 module.exports.Activate = CommentModule;
