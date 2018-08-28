86e928c190ab7e1c37e9faf4e34852e1a8bf08ff
PaoDLR <36217674+PaoDLR@users.noreply.github.com>
2018-08-28 08:51:41 +0800

    heroku connectionBase fix

--- model/connectionBase.js
+++ model/connectionBase.js
@@ -1,6 +1,6 @@
 //Create a module made only for the base connection. This way there is only 1
 //major connection created
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://hotdawg:hotdawg123@ds233452.mlab.com:33452/hotdawgdb');
+mongoose.connect('mongodb://hotdawg:hotdawg123@ds133762.mlab.com:33762/hotdawgdb');
 
 module.exports.connection = mongoose;
